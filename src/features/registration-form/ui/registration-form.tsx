320a9723e2070832fc2d399691897fa4a838ce78
Irina Kievskaya <kievskayaira23@gmail.com>
2026-02-18 16:44:29 +0700

    feat: изменен компонент input, реализована проверка введенныз данных перед регистрацией пользователя

--- src/features/registration-form/ui/registration-form.tsx
+++ src/features/registration-form/ui/registration-form.tsx
@@ -12,16 +12,24 @@ export const RegistrationForm = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [password2, setPassword2] = useState('');
+    const [clickName, setClickName] = useState(false)
+    const [clickEmail, setClickEmail] = useState(false)
+    const [clickPass, setClickPass] = useState(false)
+    const [clickPass2, setClickPass2] = useState(false)
 
     const onRegister = async (event: { preventDefault: () => void }) => {
         event.preventDefault();
-        setIsAuth(true);
-        if (password === password2) {
-            register({email, password, name})
-        } else {
-            // eslint-disable-next-line no-console
-            console.error('пароли не совпадают')
-            // TODO:
+        setClickName(true)
+        setClickEmail(true)
+        setClickPass(true)
+        setClickPass2(true)
+        if (
+            name.length >= 1 &&
+            email.includes('@') && email.includes('.') &&
+            password.length >= 6 && password === password2
+        ) {
+            register({ email, password, name })
+            setIsAuth(true);
         }
     }
     return (
@@ -30,10 +38,40 @@ export const RegistrationForm = () => {
                 <Column gap={40} alignItems="center">
                     <h1 className={styles.logo}>outfitly</h1>
                     <Column gap={12}>
-                        <Input value={name} onInput={(e) => setName(e.currentTarget.value)} placeholder="Введите имя" />
-                        <Input value={email} onInput={(e) => setEmail(e.currentTarget.value)} placeholder="Введите email" />
-                        <Input value={password} onInput={(e) => setPassword(e.currentTarget.value)} placeholder="Введите пароль" />
-                        <Input value={password2} onInput={(e) => setPassword2(e.currentTarget.value)} placeholder="Введите пароль еще раз" />
+                        <Input 
+                            onBlur={() => setClickName(true)} 
+                            error={clickName && name.length < 1} //сделать так, чтобы вводить можно было только буквы
+                            errorMessage="некорретные символы" 
+                            value={name} 
+                            onInput={(e) => setName(e.currentTarget.value)} 
+                            placeholder="Введите имя" 
+                        />
+                        <Input
+                            onBlur={() => setClickEmail(true)}
+                            error={clickEmail && !(email.includes('@') && email.includes('.'))}
+                            errorMessage="некорретный email"
+                            value={email}
+                            onInput={(e) => setEmail(e.currentTarget.value)}
+                            placeholder="Введите email"
+                        />
+                        <Input 
+                            onBlur={() => setClickPass(true)} 
+                            error={clickPass && password.length < 6} 
+                            errorMessage="минимум 6 символов" 
+                            value={password} 
+                            onInput={(e) => setPassword(e.currentTarget.value)} 
+                            placeholder="Введите пароль" 
+                            type="password"
+                        />
+                        <Input 
+                            onBlur={() => setClickPass2(true)} 
+                            error={clickPass2 && password !== password2} 
+                            errorMessage="пароли не совпадают" 
+                            value={password2} 
+                            onInput={(e) => setPassword2(e.currentTarget.value)} 
+                            placeholder="Введите пароль еще раз"                             
+                            type="password"
+                        />
                     </Column>
                 </Column>
                 <Row gap={21}>
--- src/shared/ui/input/input.tsx
+++ src/shared/ui/input/input.tsx
@@ -1,12 +1,18 @@
 import type { FC, InputHTMLAttributes } from "react";
+import clsx from "clsx";
 import styles from './style.module.css'
 
 type InputProps = InputHTMLAttributes<HTMLInputElement> & {
     placeholder: string;
+    error?: boolean;
+    errorMessage?: string;
 }
 
-export const Input: FC<InputProps> = ({placeholder, ...props}) => {
+export const Input: FC<InputProps> = ({placeholder, error, errorMessage, ...props}) => {
     return (
-        <input {...props} className={styles.input} type="text" placeholder={placeholder}/>
+        <div>
+            <input {...props} className={clsx(styles.input, error && styles.error)} placeholder={placeholder}/>
+            {error && errorMessage && <div className={styles.errorMessage}>{errorMessage}</div>}
+        </div>
     )
 }
