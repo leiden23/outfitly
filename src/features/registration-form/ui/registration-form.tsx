6fecaa17d55d58fde224624a246a46375f8bfbc5
Irina Kievskaya <kievskayaira23@gmail.com>
2026-02-19 16:21:44 +0700

    feat: добавлены регулярные выражения для имени, почты и пароля, созданы состояния валидности для каждого поля, включающие в себе правила для этих же полей

--- src/features/registration-form/ui/registration-form.tsx
+++ src/features/registration-form/ui/registration-form.tsx
@@ -1,10 +1,13 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
 import { NavLink } from "react-router-dom"
 import { register } from "../api/register"
 import styles from './style.module.css'
 import { Button, Card, Column, Input, Row } from "@/shared"
 import { useAuth } from "@/entities/user"
 
+const nameRegex = /^[a-zA-Zа-яА-Я\s]+$/;
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const passRegex = /^.{6,}$/;
 
 export const RegistrationForm = () => {
     const { setIsAuth } = useAuth()
@@ -17,6 +20,23 @@ export const RegistrationForm = () => {
     const [clickPass, setClickPass] = useState(false)
     const [clickPass2, setClickPass2] = useState(false)
 
+    const isNameValid = useMemo(() => 
+        !!name && nameRegex.test(name)
+    , [name])
+
+    const isEmailValid = useMemo(() => 
+        !!email && emailRegex.test(email)
+    , [email])
+
+    const isPassword1Valid = useMemo(() =>
+        !!password && passRegex.test(password)
+    , [password])
+
+    const isPassword2Valid = useMemo(() =>
+        !!password2 && password === password2 && passRegex.test(password2)
+    , [password, password2])
+
+
     const onRegister = async (event: { preventDefault: () => void }) => {
         event.preventDefault();
         setClickName(true)
@@ -24,14 +44,15 @@ export const RegistrationForm = () => {
         setClickPass(true)
         setClickPass2(true)
         if (
-            name.length >= 1 &&
-            email.includes('@') && email.includes('.') &&
-            password.length >= 6 && password === password2
-        ) {
+            nameRegex.test(name) && emailRegex.test(email) && 
+            passRegex.test(password) && password === password2
+        ) 
+        {
             register({ email, password, name })
             setIsAuth(true);
         }
     }
+
     return (
         <Card className={styles.form}>
             <Column gap={20} alignItems="center">
@@ -40,23 +61,25 @@ export const RegistrationForm = () => {
                     <Column gap={12}>
                         <Input 
                             onBlur={() => setClickName(true)} 
-                            error={clickName && name.length < 1} //сделать так, чтобы вводить можно было только буквы
+                            error={clickName && !isNameValid}
                             errorMessage="некорретные символы" 
                             value={name} 
                             onInput={(e) => setName(e.currentTarget.value)} 
                             placeholder="Введите имя" 
+                            type="text"
                         />
                         <Input
                             onBlur={() => setClickEmail(true)}
-                            error={clickEmail && !(email.includes('@') && email.includes('.'))}
+                            error={clickEmail && !isEmailValid}
                             errorMessage="некорретный email"
                             value={email}
                             onInput={(e) => setEmail(e.currentTarget.value)}
                             placeholder="Введите email"
+                            type="email"
                         />
                         <Input 
                             onBlur={() => setClickPass(true)} 
-                            error={clickPass && password.length < 6} 
+                            error={clickPass && !isPassword1Valid} 
                             errorMessage="минимум 6 символов" 
                             value={password} 
                             onInput={(e) => setPassword(e.currentTarget.value)} 
@@ -65,7 +88,7 @@ export const RegistrationForm = () => {
                         />
                         <Input 
                             onBlur={() => setClickPass2(true)} 
-                            error={clickPass2 && password !== password2} 
+                            error={clickPass2 && !isPassword2Valid}
                             errorMessage="пароли не совпадают" 
                             value={password2} 
                             onInput={(e) => setPassword2(e.currentTarget.value)} 
